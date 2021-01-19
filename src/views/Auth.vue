eec0e4e3f0e30db79a4ea500b222c5f2ce0709c7
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 15:25:29 +0700

    add: login form & auth views

---
+++ src/views/Auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <login-form @handleLogin="handleLogin"/>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+export default {
+  name: 'Auth',
+  components: { LoginForm },
+  methods: {
+    handleLogin (payload) {
+      this.$emit('loginData', payload)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
