49b5189cd1548fcc0f3162dcf4100d97f3cad768
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 15:27:41 +0700

    add: init product list component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ product.name }}</h2>
+    <h2>{{ product.price }}</h2>
+    <h2>{{ product.stock }}</h2>
+    <img :src="product.image_url" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductList',
+  props: ['product']
+}
+</script>
+
+<style>
+
+</style>
