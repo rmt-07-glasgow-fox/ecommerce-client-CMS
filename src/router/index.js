39568152e2621d93d3581409aaeaf0e65359539a
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 15:29:56 +0700

    add: navigation guard if user logged in

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Auth from '../views/Auth.vue'
+import Page404 from '../views/Page404.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Auth',
+    component: Auth
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  {
+    path: '*',
+    name: 'Page404',
+    component: Page404
   }
 ]
 
@@ -24,6 +28,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('access_token')
+  if ((to.name !== 'Login' && to.path !== '/') && !isAuthenticated) next({ name: 'Auth' })
+  else if ((to.name === 'Login' || to.path === '/') && isAuthenticated) next({ name: 'Dashboard' })
+  else next()
 })
 
 export default router
