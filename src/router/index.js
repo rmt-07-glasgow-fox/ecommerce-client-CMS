9a1a9965e694522298ed93c6e277ed2c3d496eca
ical10 <mochhusnir@gmail.com>
2021-01-19 16:32:33 +0700

    add NotFound view and routing for * and /dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/dashboard',
+    name: 'Dashboard',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue')
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class='container'>
+    <h1> 404 Ooopss... Not Found </h1>
+  </div>
+</template>
