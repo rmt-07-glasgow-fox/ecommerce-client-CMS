dbce898d7a1a4d96831423ebc2e0d31484b33242
imam hermawan <immd.flores@gmail.com>
2021-01-19 22:47:42 +0700

    initialize client cms

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,92 @@
+<template>
+  <section>
+    <div class="row">
+      <div class="col-2 sidebar">
+        <header>CMS</header>
+        <ul>
+          <li><a href="#"><i class="fas fa-home"></i> Dashboard</a></li>
+          <li><a href="#"><i class="fas fa-cart-plus"></i> Add Product</a></li>
+          <li><a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+        </ul>
+      </div>
+      <div class="col-10 dashboard">
+        <div class="container p-1">
+          <h2>Dashboard</h2>
+          <hr>
+        </div>
+        <div class="d-flex flex-column align-items-center">
+          <div class="card mb-3 rounded" style="width: 800px;">
+            <div class="row g-0 align-items-center">
+              <div class="col-md-5">
+                <img src="https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg" height="160px" width="300px" style="object-fit: cover;">
+              </div>
+              <div class="col-md-4">
+                <div class="card-body">
+                  <h5 class="card-title">Product Name</h5>
+                  <p class="card-text">Price: Rp. 500.000</p>
+                  <p class="card-text"><small class="text-muted">Edit Delete</small></p>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="card-body" style="text-align: right;">
+                  <p class="m-4">Stock: 100 Pcs</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+.dashboard {
+  height: 100vh;
+}
+/* sidebar */
+.sidebar {
+  background-color: rgb(31, 31, 31);
+  height: 100vh;
+  list-style: none;
+  text-decoration: none;
+}
+.sidebar header {
+  color: white;
+  font-size: 22px;
+  text-align: center;
+  line-height: 70px;
+  user-select: none;
+}
+.sidebar ul li {
+  list-style: none;
+}
+
+.sidebar ul li a{
+  text-decoration: none;
+}
+.sidebar ul {
+  padding: 0;
+}
+.sidebar ul a {
+  display: block;
+  height: 100%;
+  width: 100%;
+  line-height: 50px;
+  font-size: 15px;
+  color: white;
+  padding-left: 30px;
+  box-sizing: border-box;
+  border-top: 1px solid rgba(255,255,255,.1);
+  border-bottom: 1px solid black;
+  transition: .3s;
+}
+
+.sidebar ul a:hover {
+  padding-left: 40px;
+}
+.sidebar ul a i {
+  margin-right: 10px;
+}
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,90 @@
+<template>
+  <section id="login-page">
+    <div class="center">
+      <!-- <h1 style="margin-bottom: 3rem;" class="welcome">E-COMMERCE CMS</h1> -->
+      <div class="container-fluid d-flex flex-row">
+        <!-- LOGIN FORM -->
+        <div id="login-form-page">
+          <div class="container d-flex justify-content-center">
+            <form style="width: 20rem;">
+              <div class="mb-3">
+                <h2 class="white">Sign In</h2>
+              </div>
+              <div class="mb-3">
+                <label for="email" class="white form-label">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                <div id="emailHelp" class="white form-text">Whether you speak secretly or openly—He surely knows best what is ˹hidden˺ in the heart.</div>
+              </div>
+              <div class="mb-3">
+                <label for="password" class="white form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+              </div>
+              <button type="submit" class="btn btn-light" id="login-btn">Sign in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.box {
+  height: 550px;
+}
+
+.box1 {
+  height: 100%;
+  overflow: auto;
+  margin-bottom: 10px;
+}
+
+.description p {
+  font-size: 10px;
+  margin: 0;
+}
+
+.edit {
+  padding: 0.1px;
+}
+
+.fa-plus {
+  font-size: 20px;
+}
+
+#login-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,0,0,1) 55%, rgba(255,255,255,1) 100%);
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+.welcome {
+  color: white;
+  text-shadow: 2px 5px 10px black;
+}
+.white{
+  color: white;
+}
+</style>
