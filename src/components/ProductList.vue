2027be68f126d5335750085c9bfe61f9340a7c29
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 17:09:28 +0700

    add: maskPrice and maskStock

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
-    <h2>{{ product.name }}</h2>
-    <h2>{{ product.price }}</h2>
-    <h2>{{ product.stock }}</h2>
-    <img :src="product.image_url" alt="">
-  </div>
+  <tr>
+    <th> {{ product.id }} </th>
+    <td><img :src="product.image_url" alt="" class="img-product"></td>
+    <td> {{ product.name }}</td>
+    <td> {{ maskPrice }}</td>
+    <td> {{ maskStock }} </td>
+    <td> <button class="btn btn-warning">Edit</button> <button type="button" class="btn btn-danger">Delete</button></td>
+  </tr>
 </template>
 
 <script>
 export default {
   name: 'ProductList',
-  props: ['product']
+  props: ['product'],
+  computed: {
+    maskPrice () {
+      return `Rp. ${Number(this.product.price).toLocaleString('id-ID')}`
+    },
+    maskStock () {
+      return Number(this.product.stock).toLocaleString('id-ID')
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+}
 
 </style>
