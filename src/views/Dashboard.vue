0a83f7a41134283d2b62fc1b423bc75291eddd86
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 17:09:54 +0700

    add: v-for table data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <navbar @handleLogout="handleLogout"/>
-    <div v-for="product in products" :key="product.id">
-      <product-list :product="product"/>
-    </div>
+    <section class="table-product container mt-3 text-center">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Image</th>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <product-list v-for="product in products" :key="product.id" :product="product" />
+        </tbody>
+      </table>
+    </section>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
 import ProductList from '../components/ProductList.vue'
+
 export default {
   name: 'Dashboard',
   components: { Navbar, ProductList },
