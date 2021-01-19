669e578ca4757e7fddde3c107f940612f420bebd
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 15:27:00 +0700

    add: navbar and emit logout button to parent

---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <div>
+      <router-link class="nav-child" to="/">Home</router-link>
+      <router-link class="nav-child" to="/products">Products</router-link>
+      <router-link class="nav-child" to="/banners">Banners</router-link>
+      <router-link class="nav-child" to="/categories">Categories</router-link>
+    </div>
+    <div>
+      <a href="#" @click="handleLogout">Logout</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    handleLogout () {
+      this.$emit('handleLogout', true)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+#nav {
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #a6e2e2;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.nav-child {
+  margin-right: 10px;
+}
+
+</style>
