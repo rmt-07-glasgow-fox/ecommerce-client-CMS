64dfe5c17d832fbbffe883c5fe695e4d4cae4cef
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 15:27:15 +0700

    add: dashboard init

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <navbar @handleLogout="handleLogout"/>
+    <div v-for="product in products" :key="product.id">
+      <product-list :product="product"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import ProductList from '../components/ProductList.vue'
+export default {
+  name: 'Dashboard',
+  components: { Navbar, ProductList },
+  props: ['products'],
+  methods: {
+    handleLogout (payload) {
+      this.$emit('handleLogout', payload)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
