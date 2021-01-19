5ebedc3cba1f6a5f1ea9afd6d3f15946a09740e9
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-19 17:09:04 +0700

    disable background color

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  background-color: #e4e3e3;
+  /* background-color: #e4e3e3; */
 }
 
 </style>
