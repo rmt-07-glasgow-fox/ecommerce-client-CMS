9dd7ad3995c877fd9d31f537deabee013c8c08f4
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-01-20 10:48:18 +0700

    Making The Client CMS

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" :src="poster" height="300px">
+    <HelloWorld msg="Welcome to Munthe E-Commerce"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      poster: 'https://lovelive-store.bnarts.jp/assets/img/mainvisual-1_pc.png'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
