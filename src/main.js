ef8206a611f5dfb4290cd0442f5b50ee738df398
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-20 12:16:42 +0700

    add: vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
