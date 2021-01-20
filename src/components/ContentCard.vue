893b57da991bda930352d546a03130d4d4fa8eac
ical10 <mochhusnir@gmail.com>
2021-01-20 21:52:53 +0700

    content card show details on click

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -6,14 +6,13 @@
     <div class='row'>
       <div class='col-3 pl-0 pr-0 mb-4' v-for='product in products' :key='product.id'>
         <div class='card'>
-          <img :src="`${product.image_url}`" class="card-img-top img-fluid" alt="image not found">
+          <img :src="`${product.image_url}`" class="card-img-top img-fluid" alt="image not found" @click='productDetail(product.id)'>
           <div class='card-header'>
             <p class='text-weight-bold'>{{ product.name }}</p>
           </div>
           <div class="card-body">
             <p class="card-text">Stock: {{ product.stock }}</p>
-            <p class="card-text">Price: {{ product.price }}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text">Price: {{ formatPrice(product.price) }}</p>
           </div>
         </div>
       </div>
@@ -24,6 +23,19 @@
 <script>
 export default {
   name: 'ContentCard',
+  methods: {
+    formatPrice (price) {
+      return 'IDR ' + price.toLocaleString('id')
+    },
+    productDetail (id) {
+      this.$router.push({
+        name: 'ProductDetail',
+        params: {
+          id
+        }
+      })
+    }
+  },
   computed: {
     products () {
       return this.$store.state.products
