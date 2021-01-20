b88b50f787c0b33e0e5985cea8924c314d246b2b
ical10 <mochhusnir@gmail.com>
2021-01-20 18:52:11 +0700

    styling ContentCard and Dashboard

---
+++ src/components/ContentCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class='container'>
+    <div class='row'>
+      <h1>Dashboard</h1>
+    </div>
+    <div class='row'>
+      <div class='col-3 pl-0 pr-0 mb-4' v-for='product in products' :key='product.id'>
+        <div class='card'>
+          <img :src="`${product.image_url}`" class="card-img-top img-fluid" alt="image not found">
+          <div class='card-header'>
+            <p class='text-weight-bold'>{{ product.name }}</p>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Stock: {{ product.stock }}</p>
+            <p class="card-text">Price: {{ product.price }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentCard',
+  computed: {
+    products () {
+      return this.$store.state.products
+    }
+  },
+  created () {
+    this.$store.dispatch('fetchProducts')
+  }
+}
+</script>
+
+<style scoped>
+.card-img-top {
+    width: 100%;
+    height: 15vw;
+    object-fit: cover;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="dashboard">
-    <NavbarDashboard />
-    <h1>Dashboard</h1>
+    <div class='container'>
+      <NavbarDashboard />
+    </div>
+    <div class="container">
+      <ContentCard />
+    </div>
   </div>
 </template>
 
 <script>
 import NavbarDashboard from '../components/NavbarDashboard'
+import ContentCard from '../components/ContentCard'
 export default {
   components: {
-    NavbarDashboard
+    NavbarDashboard,
+    ContentCard
   }
 }
 </script>
