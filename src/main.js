bb0fd56b639b8d0fb8bd3f9737f3a1b773eee746
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-20 17:40:30 +0700

    add: vue sweetalert

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from './store'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueSweetalert2)
 Vue.config.productionTip = false
 
 new Vue({
