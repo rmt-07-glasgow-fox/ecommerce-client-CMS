a2d6b198ce7f70f058c45700bf90475841f1a6e2
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-01-20 12:17:32 +0700

    add layout

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,128 @@
+<template>
+  <div :class="['d-flex', toggled ? '' : 'toggled']" id="wrapper">
+    <!-- Sidebar -->
+    <div class="border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading border-bottom text-center">E-Commerce CMS</div>
+      <div class="list-group list-group-flush border-bottom">
+        <a href="#" class="list-group-item list-group-item-action"
+          ><i class="fa fa-home mr-2"></i> Home</a
+        >
+        <a href="#" class="list-group-item list-group-item-action"
+          ><i class="fa fa-picture-o mr-2"></i> Banner</a
+        >
+        <a href="#" class="list-group-item list-group-item-action"
+          ><i class="fa fa-folder mr-2"></i> Category</a
+        >
+        <a href="#" class="list-group-item list-group-item-action"
+          ><i class="fa fa-product-hunt mr-2"></i> Product</a
+        >
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <a class="text-right" href="#" @click="toggled = !toggled"
+          ><i :class="['fa', toggled ? 'fa-toggle-on' : 'fa-toggle-off']" aria-hidden="true"></i
+        ></a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Account
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid">
+        <h1 class="mt-4">Home</h1>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      toggled: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+body {
+  overflow-x: hidden;
+}
+
+#sidebar-wrapper {
+  min-height: 100vh;
+  margin-left: -15rem;
+  -webkit-transition: margin 0.25s ease-out;
+  -moz-transition: margin 0.25s ease-out;
+  -o-transition: margin 0.25s ease-out;
+  transition: margin 0.25s ease-out;
+}
+
+#sidebar-wrapper .sidebar-heading {
+  padding: 0.875rem 1.25rem;
+  font-size: 1.2rem;
+}
+
+#sidebar-wrapper .list-group {
+  width: 15rem;
+}
+
+#page-content-wrapper {
+  min-width: 100vw;
+}
+
+#wrapper.toggled #sidebar-wrapper {
+  margin-left: 0;
+}
+
+@media (min-width: 768px) {
+  #sidebar-wrapper {
+    margin-left: 0;
+  }
+
+  #page-content-wrapper {
+    min-width: 0;
+    width: 100%;
+  }
+
+  #wrapper.toggled #sidebar-wrapper {
+    margin-left: -15rem;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container centered">
+    <form class="form card p-3">
+      <div class="text-center p-2 mb-3">
+        <h5>Login</h5>
+      </div>
+      <div class="form-group">
+        <input type="text" placeholder="Email" id="login" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input type="password" placeholder="Password" id="pass" class="form-control" />
+      </div>
+      <button type="submit" @click="toDashboard" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    toDashboard() {
+      this.$router.push('Dashboard');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.centered {
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 50%;
+  height: 50%;
+  min-width: 200px;
+  max-width: 400px;
+  padding: 40px;
+}
+</style>
