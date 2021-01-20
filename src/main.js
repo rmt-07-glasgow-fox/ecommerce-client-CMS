85fcc5b5493a6962c12de5c43079607f0f848816
rozakus <abdulrozak.mail@gmail.com>
2021-01-20 12:17:50 +0700

    setting vue

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// styling
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+Vue.use(Vuesax)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="hidden">
+      <vs-sidebar absolute open background="primary" textWhite>
+        <vs-sidebar-item id="products"> Products </vs-sidebar-item>
+        <vs-sidebar-item id="addProducts"> Add new product </vs-sidebar-item>
+        <vs-sidebar-item id="banners"> Banners </vs-sidebar-item>
+        <vs-sidebar-item id="addBanners"> Add new banner </vs-sidebar-item>
+        <vs-sidebar-item id="logout"> Logout </vs-sidebar-item>
+      </vs-sidebar>
+    </div>
+    <div>
+      <vs-table>
+        <template #thead>
+          <vs-tr>
+            <vs-th> Name </vs-th>
+            <vs-th> Email </vs-th>
+            <vs-th> Id </vs-th>
+          </vs-tr>
+        </template>
+      </vs-table>
+    </div>
+  </div>
+</template>
