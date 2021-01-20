58133ced2baadd76bce27c2af06ef9fe28fd7f44
ical10 <mochhusnir@gmail.com>
2021-01-20 14:27:38 +0700

    navigation guards for Home and Dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,28 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter (to, from, next) {
+      if (localStorage.getItem('access_token')) {
+        next({
+          name: 'Dashboard'
+        })
+      } else {
+        next()
+      }
+    }
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue')
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue'),
+    beforeEnter (to, from, next) {
+      if (localStorage.getItem('access_token')) {
+        next()
+      } else {
+        next({ name: 'Home' })
+      }
+    }
   },
   {
     path: '*',
