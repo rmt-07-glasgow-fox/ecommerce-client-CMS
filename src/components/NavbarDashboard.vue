3512b4f4e4aea2d51fb8d2d40cd847e0bdf62d4d
ical10 <mochhusnir@gmail.com>
2021-01-20 14:32:34 +0700

    creating Navbar component for Dashboard view

---
+++ src/components/NavbarDashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <button class='btn btn-sm btn-outline-danger' @click='logout'>Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      localStorage.clear()
+      this.$router.push('/')
+    }
+  }
+}
+</script>
