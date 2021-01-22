d20cf946e2a85ce61be1a72c982e07aea97b7c86
ical10 <mochhusnir@gmail.com>
2021-01-22 16:39:07 +0700

    adding a component in EditProduct

--- src/views/EditProduct.vue
+++ src/views/EditProduct.vue
@@ -1,17 +1,15 @@
 <template>
   <div class='container'>
-    <form>
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Product name..." required='' v-model='name'>
-      </div>
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Product price..." required='' v-model='price'>
-      </div>
-      <div class="form-group">
-        <label for='stock'>Number of stock: </label>
-        <input type="text" id='stock' class="form-control" required='' v-model='stock'>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <EditForm />
   </div>
 </template>
+
+<script>
+import EditForm from '../components/EditForm'
+export default {
+  name: 'EditProduct',
+  components: {
+    EditForm
+  }
+}
+</script>
