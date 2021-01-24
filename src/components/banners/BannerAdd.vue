dbf3300a961ce807b12eb0b943fcf1acbcfcf95c
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-01-24 12:32:17 +0700

    CRUD Banner Done

---
+++ src/components/banners/BannerAdd.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Add Banner</h1>
+    <form @submit.prevent="addBanner">
+      <div class="mb-1">
+        <label for="name" class="col-form-label">Banner Title</label>
+        <input
+          v-model="payload.name"
+          type="text"
+          class="form-control"
+          id="name"
+        />
+      </div>
+
+      <div class="mb-1">
+        <label for="image_url" class="col-form-label">Image URL</label>
+        <input
+          v-model="payload.image_url"
+          type="text"
+          class="form-control"
+          id="image_url"
+        />
+      </div>
+
+      <div class="mt-3 mb-3">
+        <button type="submit" class="btn btn-sm btn-primary">Add</button>
+        <router-link
+          to="/admindashboard/banners"
+          style="text-decoration: none"
+        >
+          <button
+            type="button"
+            style="margin-left: 36px"
+            class="btn btn-sm btn-danger"
+          >
+            Cancel
+          </button>
+        </router-link>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddBanner',
+  data () {
+    return {
+      payload: {
+        name: '',
+        image_url: ''
+      }
+    }
+  },
+  methods: {
+    addBanner () {
+      this.$store.dispatch('addBanner', this.payload)
+    }
+  }
+}
+</script>
---
+++ src/components/banners/BannerCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="card shadow mb-3"
+    style="border-radius: 8px; margin-left: auto; margin-right: auto"
+  >
+    <img
+      :src="Banner.image_url"
+      class="card-img-top"
+      style="
+        height: 320px
+        overflow: hidden;
+        align-item: center;
+        object-fit: cover;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+      "
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ Banner.name }}</h5>
+      <router-link :to="`/admindashboard/banners/edit/${Banner.id}`">
+        <button
+          href=""
+          class="btn btn-outline-primary"
+          style="margin-right: 32px"
+        >
+          Edit
+        </button>
+      </router-link>
+      <button
+        @click="deleteBanner(Banner.id)"
+        href=""
+        class="btn btn-outline-danger"
+      >
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BannerCard',
+  props: ['Banner'],
+  methods: {
+    deleteBanner (id) {
+      this.$store.dispatch('deleteBanner', id)
+    }
+  }
+}
+</script>
