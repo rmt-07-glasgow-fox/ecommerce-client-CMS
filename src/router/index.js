c63db9d3de0b337ec40132c04f87a9a794e1ce0d
Mochammad Andrian Maulana <andrianmaulana28@gmail.com>
2021-01-24 17:52:33 +0700

    Sidebar; Navbar; BProducts; Categories; Banners;

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,13 @@ const routes = [
     component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
