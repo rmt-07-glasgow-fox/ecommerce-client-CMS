1e13198468e0705e7ffa35e966c67c9df85bc511
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2021-01-24 17:52:36 +0700

    change server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  // baseURL: 'http://localhost:3000'
+  baseURL: 'https://e-commerce-server-hanii.herokuapp.com'
 })
 
 export default instance
