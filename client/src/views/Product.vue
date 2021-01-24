21a3db26d3b5164258874958a718260bc64d74c4
ahmad-akbar <ahmd.akbr25@gmail.com>
2021-01-24 12:13:17 +0800

    read data from server

---
+++ client/src/views/Product.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="class container">
+  <div class="product">
+    <h1>List of Product</h1>
+    <!-- Product Card -->
+    <div class="row">
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"/>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from '../api/axios'
+import ProductCard from '../components/ProductCard.vue'
+
+export default {
+  name: 'Product',
+  components: {
+    ProductCard
+  },
+  data () {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    fetchProduct () {
+      axios
+        .get('/product')
+        .then(({ data }) => {
+          this.products = data
+        })
+        .catch(err => console.log(err))
+    }
+  },
+  created () {
+    this.fetchProduct()
+  }
+}
+</script>
