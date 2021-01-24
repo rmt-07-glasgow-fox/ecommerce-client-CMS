1f023cf145211c4fdf81aedd5119bd789d00546e
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-24 15:00:02 +0700

    [nanda] update file api to deploy & fix bug lint

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from '../store.js'
 
 const $axios = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://nikecommers.herokuapp.com',
   headers: {
     'Content-Type': 'application/json'
   }
