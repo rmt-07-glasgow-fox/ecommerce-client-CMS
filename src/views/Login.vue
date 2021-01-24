701e539c9b3fd9530185d7642c204b1d4a903d85
rozakus <abdulrozak.mail@gmail.com>
2021-01-24 15:19:00 +0700

    final styling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="vh-100 vw-100 bg-info">
-    <div class="container card">
-      <h4>Login</h4>
-      <form @submit.prevent="login">
-        <div class="form-group">
-          <label>Email address</label>
+  <div class="vh-100 vw-100 bg-info p-5">
+    <div class="container card text-center h-80 p-5">
+      <h4 class="my-3">Admin Login E-commerce</h4>
+      <form @submit.prevent="login" class="my-5">
+        <div class="form-group d-flex flex-row">
+          <label class="mx-5">Email address</label>
           <input v-model="inputLogin.email" type="email" class="form-control" />
         </div>
-        <div class="form-group">
-          <label>Password</label>
+        <div class="form-group d-flex flex-row">
+          <label class="mx-5">Password</label>
           <input
             v-model="inputLogin.password"
             type="password"
