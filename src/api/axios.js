3ed798e9f542ea32f7c3256a48ff10ba21bf7262
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-28 12:16:21 +0700

    edit-baseurl-api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'https://radiant-headland-12217.herokuapp.com/'
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://radiant-headland-12217.herokuapp.com/'
+  // baseURL: 'http://localhost:3000'
 })
 
 export default instance
