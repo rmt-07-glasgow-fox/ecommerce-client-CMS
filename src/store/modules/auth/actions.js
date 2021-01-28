ee67b90a53d242ffe7064b099500b8061c460bf0
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-01-28 12:34:33 +0700

    change endpoint login admin

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -3,7 +3,7 @@ import axios from '@/api/axios';
 export default {
   async login(context, payload) {
     try {
-      const response = await axios.post('/users/login', payload);
+      const response = await axios.post('/users/login/admin', payload);
       localStorage.setItem('access_token', response.data.access_token);
       context.commit('setUser', {
         access_token: response.data.access_token,
