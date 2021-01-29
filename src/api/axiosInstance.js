4dbabcd32a56875f298d9a02fab83c78d4081c5d
ical10 <mochhusnir@gmail.com>
2021-01-29 11:26:05 +0700

    change baseurl before deployment

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
-  // baseURL: 'https://ecommerce-cms-esd20.herokuapp.com',
+  baseURL: 'https://ecommerce-cms-esd20.herokuapp.com'
 })
 
 export default instance
